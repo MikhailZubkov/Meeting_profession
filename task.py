878253f1802d96037a6d38bf92ca4fbe89cbacde
MikhailZubkov <toothkov@gmail.com>
2023-05-25 20:10:33 +0500

    updated readme, added code

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
-list1 = list(map(str, input('введите через пробел элементы массива ').split()))
-for i in range(len(list1)-1):
-    if len(list1[i]) > 3:
-        list1.pop(i)
-print(list1)
-
-#print([i for i in (list(map(str, input().split()))) if len(i) <= 3])
+list1 = list(map(str, input('введите элементы массива через пробел: ').split()))
+list2 = []
+for i in range(len(list1)):
+    if len(list1[i]) <= 3:
+        list2.append(list1[i])
+print(list2)
+#print([i for i in (list(map(str, input('введите элементы массива через пробел: ').split()))) if len(i) <= 3])
